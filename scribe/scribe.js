c3a988ede8aa6450783af97aab951b7b237eec52
Jeffrey Lou <jeffzlou@gmail.com>
2018-09-16 16:39:26 -0400

    wip js scribe, added wait for rabbit but it is not working

--- scribe/scribe.js
+++ scribe/scribe.js
@@ -1,1 +1,27 @@
+require('dotenv').config();
+const Influx = require('influx');
+const amqp = require('amqplib/callback_api');
 
+const influx = new Influx.InfluxDB({
+  host: 'influxdb',
+  database: 'weather',
+});
+
+influx.getDatabaseNames()
+  .then(names => {
+    if (!names.includes('weather')) {
+      return influx.createDatabase('weather');
+    }
+  })
+  .then(() => {console.log('_____connected to influx')})
+
+amqp.connect('amqp://rabbitmq', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    let q = 'hello';
+    ch.assertQueue(q, {durable: false});
+    console.log("waiting for messages")
+    ch.consume(q, function(msg) {
+      console.log(`received: ${msg.content.toString()}`)
+    }, {noAck: true});
+  });
+});

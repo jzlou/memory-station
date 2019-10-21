a7ad2f123c149dfaaf72196f124bda7821abc5ec
jeff lou <jeffzlou@gmail.com>
2019-10-20 23:35:42 -0400

    add some security to the API server, like helmet and ipfilter

--- api.js
+++ api.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3000;
 api.use(bodyparser.json());
 api.use(bodyparser.urlencoded({extended: false}));
 
+api.post('/', (req, res) => {
+  return res.send('received a POST HTTP method');
+});
+
+api.post('/:payload', (req, res) => {
+  return res.send(`the payload: ${req.params.payload}`);
+});
+
 api.listen(port, () => {
   console.log(`api engine listening on port ${port}`);
 });

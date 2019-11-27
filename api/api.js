19d48081a229f7969f47c43f66d0184892dfc20b
Jeff Lou <jeffzlou@gmail.com>
2019-11-27 11:26:14 -0500

    migrate to pg with timescaleDB, remove influx.

--- api/api.js
+++ api/api.js
@@ -1,18 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const bodyparser = require('body-parser');
-const Influx = require('influx');
 const helmet = require('helmet');
 const Sentry = require('@sentry/node');
 const morgan = require('morgan');
+const Knex = require('knex');
 
 const api = express();
 const port = process.env.API_PORT || 3000;
-const influx = new Influx.InfluxDB({
-  host: process.env.INFLUX_HOST
-})
-const allowed_ips = [process.env.IP];
 Sentry.init({ 'dsn': process.env.SENTRY_DSN });
+const knex = Knex({
+  client: 'pg',
+  version: process.env.PG_VERSION,
+  connection: {
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+  }
+})
 
 api.use(helmet());
 api.use(bodyparser.json());
@@ -23,21 +29,6 @@ api.use(Sentry.Handlers.errorHandler());
 api.set('secret', process.env.SECRET);
 
 api.post('/', (req, res) => {
-  influx.writePoints([
-    {
-      measurement: req.body.measurement,
-      tags: req.body.tags,
-      fields: req.body.fields,
-      timestamp: req.body.timestamp
-    }
-  ]).catch(
-    error => {
-      setTimeout(() => { throw error; });
-    }
-  );
-  res.status(200).send(
-    `successfully wrote data point with ts ${req.body.timestamp}`
-  );
 });
 
 api.get('/', (req, res) => {

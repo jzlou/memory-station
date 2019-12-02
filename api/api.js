5de848223ac8234be033fa9f1bc846446518389d
Jeff Lou <jeffzlou@gmail.com>
2019-12-01 23:08:56 -0500

    use convenience method, pool.query()

--- api/api.js
+++ api/api.js
@@ -12,6 +12,10 @@ Sentry.init({ 'dsn': process.env.SENTRY_DSN });
 const pool = new Pool({
   connectionString: process.env.PG_CONNECTION
 })
+pool.on('error', (err, client) => {
+  console.error('unexpected error on idle client', err);
+  process.exit(-1);
+});
 
 api.use(helmet());
 api.use(bodyparser.json());
@@ -26,23 +30,14 @@ api.post('/', (req, res) => {
 
 // retrieve locaiton records
 api.get('/locations/:id', (req, res) => {
-  pool.connect((err, client, done) => {
-    // handle errors
+  pool.query('SELECT * FROM locations WHERE id = $1;', [req.params.id], (err, result) => {
     if (err) {
-      done();
-      console.log(err);
+      console.error(err.stack);
+      return res.status(500).json({success: false, data: err});
+    } else {
+      console.log(result.rows);
+      return res.send(result.rows);
     }
-
-    client.query('SELECT * FROM locations WHERE id = $1;', [req.params.id], (err, result) => {
-      done()
-      if (err) {
-        console.log(err.stack);
-        return res.status(500).json({success: false, data: err});
-      } else {
-        console.log(result.rows);
-        return res.send(result.rows);
-      }
-    });
   });
 });
 
